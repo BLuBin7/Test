4728be98365c234f35a52c11a19f21f50508f9f7
BLuBin7 <121689786+BLuBin7@users.noreply.github.com>
2023-01-27 09:43:34 +0700

    Update DrawWithTurtle.py

--- DrawWithTurtle.py
+++ DrawWithTurtle.py
@@ -4,41 +4,41 @@ turtle.bgcolor("black")
 size = 20
 circles = 20
 turtle.speed(300)
-
+@BLuBin
 turtle.colormode(255)
-
+#move 
 def move(length, angle):
-                turtle.right(angle)
-                turtle.forward(length)
-
+   turtle.right(angle)
+   turtle.forward(length)
+#random
 def hex():
-        turtle.pendown()                        
-        turtle.color( randint(0,255),randint(0,255),randint(0,255) )
-        turtle.begin_fill()
-        for i in range(6):
-                move(size,-60)
-        turtle.end_fill()
-        turtle.penup()
+    turtle.pendown()                        
+    turtle.color( randint(0,255),randint(0,255),randint(0,255) )
+    turtle.begin_fill()
+    for i in range(6):
+            move(size,-60)
+    turtle.end_fill()
+    turtle.penup()
 
 # start
 turtle.penup()
 
 for circle in range (circles):
-        if circle == 0:
-                hex()
-                move(size,-60)
-                move(size,-60)
-                move(size,-60)
-                move(0,180)
-        for i in range (6):
-                move(0,60)
-                for j in range (circle+1):
-                        hex()
-                        move(size,-60)
-                        move(size,60)
-                move(-size,0)
-        move(-size,60)
-        move(size,-120)
-        move(0,60)
+    if circle == 0:
+            hex()
+            move(size,-60)
+            move(size,-60)
+            move(size,-60)
+            move(0,180)
+    for i in range (6):
+            move(0,60)
+            for j in range (circle+1):
+                    hex()
+                    move(size,-60)
+                    move(size,60)
+            move(-size,0)
+    move(-size,60)
+    move(size,-120)
+    move(0,60)
 
 turtle.exitonclick()
